d0cba0d6cab03584981625404d341b63298064f5
Theodor Johansson <theodor.johansson@praetorian.com>
2021-06-29 14:21:22 -0500

    Initial Commit

---
+++ src/main/java/praetorian/vulnviewer/vulnViewer/VulnViewerApplication.java
@@ -0,0 +1,13 @@
+package praetorian.vulnviewer.vulnViewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VulnViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VulnViewerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/praetorian/vulnviewer/vulnViewer/VulnViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package praetorian.vulnviewer.vulnViewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VulnViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
